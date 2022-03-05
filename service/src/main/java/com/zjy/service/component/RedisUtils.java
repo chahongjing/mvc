ceacc42e231bbc537c5436672509b2c4e9b9642a
zengjunyi <zengjunyi@xiaomi.com>
2022-03-05 23:00:30 +0800

    feat: redis

--- service/src/main/java/com/zjy/service/component/RedisUtils.java
+++ service/src/main/java/com/zjy/service/component/RedisUtils.java
@@ -1,14 +1,24 @@
 package com.zjy.service.component;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
 public class RedisUtils {
+
+    private static final String SET_KEY = "mvc:cache:set";
+    private static final String ZSET_KEY = "mvc:cache:zset";
+    private static final int timeout_second = 3600;
+
     @Resource
     @Lazy
     private StringRedisTemplate stringRedisTemplate;
@@ -26,8 +36,218 @@ public class RedisUtils {
         return r != null && r;
     }
 
+    // region string
     public boolean del(String key) {
         Boolean r = stringRedisTemplate.delete(key);
         return r == null || r;
     }
+
+    public void set(String key, String value, long time) {
+        stringRedisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+    }
+
+    public void set(String key, String value) {
+        stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    public String get(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    public Long del(List<String> keys) {
+        return stringRedisTemplate.delete(keys);
+    }
+
+    public Boolean expire(String key, long time) {
+        return stringRedisTemplate.expire(key, time, TimeUnit.SECONDS);
+    }
+
+    public Long getExpire(String key) {
+        return stringRedisTemplate.getExpire(key, TimeUnit.SECONDS);
+    }
+
+    public Boolean hasKey(String key) {
+        return stringRedisTemplate.hasKey(key);
+    }
+
+    public Long incr(String key, long delta) {
+        return stringRedisTemplate.opsForValue().increment(key, delta);
+    }
+
+    public Long decr(String key, long delta) {
+        return stringRedisTemplate.opsForValue().increment(key, -delta);
+    }
+    // endregion
+
+    // region hash
+    public Object hGet(String key, String hashKey) {
+        return stringRedisTemplate.opsForHash().get(key, hashKey);
+    }
+
+    public Boolean hSet(String key, String hashKey, Object value, long time) {
+        stringRedisTemplate.opsForHash().put(key, hashKey, value);
+        return expire(key, time);
+    }
+
+    public void hSet(String key, String hashKey, Object value) {
+        stringRedisTemplate.opsForHash().put(key, hashKey, value);
+    }
+
+    public Map<Object, Object> hGetAll(String key) {
+        return stringRedisTemplate.opsForHash().entries(key);
+    }
+
+    public Boolean hSetAll(String key, Map<String, Object> map, long time) {
+        stringRedisTemplate.opsForHash().putAll(key, map);
+        return expire(key, time);
+    }
+
+    public void hSetAll(String key, Map<String, ?> map) {
+        stringRedisTemplate.opsForHash().putAll(key, map);
+    }
+
+    public void hDel(String key, Object... hashKey) {
+        stringRedisTemplate.opsForHash().delete(key, hashKey);
+    }
+
+    public Boolean hHasKey(String key, String hashKey) {
+        return stringRedisTemplate.opsForHash().hasKey(key, hashKey);
+    }
+
+    public Long hIncr(String key, String hashKey, Long delta) {
+        return stringRedisTemplate.opsForHash().increment(key, hashKey, delta);
+    }
+
+    public Long hDecr(String key, String hashKey, Long delta) {
+        return stringRedisTemplate.opsForHash().increment(key, hashKey, -delta);
+    }
+    // endregion
+
+    // region set
+    public Set<String> sMembers(String key) {
+        return stringRedisTemplate.opsForSet().members(key);
+    }
+
+    public Long sAdd(String key, String... values) {
+        return stringRedisTemplate.opsForSet().add(key, values);
+    }
+
+    public Long sAdd(String key, long time, String... values) {
+        Long count = stringRedisTemplate.opsForSet().add(key, values);
+        expire(key, time);
+        return count;
+    }
+
+    public Boolean sIsMember(String key, Object value) {
+        return stringRedisTemplate.opsForSet().isMember(key, value);
+    }
+
+    public Long sSize(String key) {
+        return stringRedisTemplate.opsForSet().size(key);
+    }
+
+    public Long sRemove(String key, Object... values) {
+        return stringRedisTemplate.opsForSet().remove(key, values);
+    }
+    // endregion
+
+    // region list
+    public List<String> lRange(String key, long start, long end) {
+        return stringRedisTemplate.opsForList().range(key, start, end);
+    }
+
+    public Long lSize(String key) {
+        return stringRedisTemplate.opsForList().size(key);
+    }
+
+    public String lIndex(String key, long index) {
+        return stringRedisTemplate.opsForList().index(key, index);
+    }
+
+    public Long lPush(String key, String value) {
+        return stringRedisTemplate.opsForList().rightPush(key, value);
+    }
+
+    public Long lPush(String key, String value, long time) {
+        Long index = stringRedisTemplate.opsForList().rightPush(key, value);
+        expire(key, time);
+        return index;
+    }
+
+    public Long lPushAll(String key, String... values) {
+        return stringRedisTemplate.opsForList().rightPushAll(key, values);
+    }
+
+    public Long lPushAll(String key, Long time, String... values) {
+        Long count = stringRedisTemplate.opsForList().rightPushAll(key, values);
+        expire(key, time);
+        return count;
+    }
+
+    public Long lRemove(String key, long count, String value) {
+        return stringRedisTemplate.opsForList().remove(key, count, value);
+    }
+    // endregion
+
+    // region zset opt
+    public void addItem(String instanceId) {
+        log.info("添加超时任务：{}", instanceId);
+        String key = getZsetCacheKey();
+        // 添加24小时超时
+        long deadline = System.currentTimeMillis() + timeout_second * 1000L;
+        stringRedisTemplate.opsForZSet().remove(key, instanceId);
+        stringRedisTemplate.opsForZSet().add(key, instanceId, deadline);
+        // 批量添加
+//        long deadline = new Date().getTime();
+//        Set<ZSetOperations.TypedTuple<String>> set = entry.getValue().stream().map(m -> new DefaultTypedTuple<String>(String.valueOf(m), (double)deadline)).collect(Collectors.toSet());
+//        redisTemplate.opsForZSet().add(key, set);
+    }
+
+    public void removeItem(String instanceId) {
+        log.info("删除超时任务：{}", instanceId);
+        String key = getZsetCacheKey();
+        stringRedisTemplate.opsForZSet().remove(key, instanceId);
+    }
+
+    public int handleTimeoutGov(int limit) {
+        log.info("开始处理超时工单");
+        String key = getZsetCacheKey();
+        Set<String> instanceIds = stringRedisTemplate.opsForZSet().rangeByScore(key, 0, System.currentTimeMillis(), 0, limit);
+        if (CollectionUtils.isEmpty(instanceIds)) {
+            return 0;
+        }
+        log.info("开始处理超时工单：{}", JSON.toJSONString(instanceIds));
+        for (String instanceId : instanceIds) {
+            log.info("handleTimeoutGov. id: {}", instanceId);
+            // todo: handle something
+//            workOrderRemindService.remindAfterOperation(tenantId, userTenantId, wo, WorkOrderOperationEnum.TIMEOUT, null, 0L);
+            removeItem(instanceId);
+        }
+        return instanceIds.size();
+    }
+
+    private String getZsetCacheKey() {
+        return ZSET_KEY;
+    }
+    // endregion
+
+    // region set opt
+    public void test(String id) {
+        String key = getSetCacheKey();
+        stringRedisTemplate.opsForSet().add(key, id);
+//        String[] idList = entry.getValue().stream().map(String::valueOf).toArray(String[]::new);
+//        stringRedisTemplate.opsForSet().add(key, idList);
+        Set<String> strings = stringRedisTemplate.opsForSet().distinctRandomMembers(key, 200L);
+//        Set<String> codeList = stringRedisTemplate.opsForSet().members(key);
+        if(strings != null) {
+            for (String item : strings) {
+                stringRedisTemplate.opsForSet().remove(key, item);
+            }
+        }
+    }
+
+    private String getSetCacheKey() {
+        return SET_KEY;
+    }
+    // endregion
 }
