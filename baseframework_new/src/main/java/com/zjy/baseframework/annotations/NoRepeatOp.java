b1c8791e86391f1efaab1f7f00e1ea34f5cef2e9
zengjunyi <zengjunyi@xiaomi.com>
2022-03-27 14:36:26 +0800

    feat: 优化

--- baseframework_new/src/main/java/com/zjy/baseframework/annotations/NoRepeatOp.java
+++ baseframework_new/src/main/java/com/zjy/baseframework/annotations/NoRepeatOp.java
@@ -1,12 +1,15 @@
-package com.zjy.common;
+package com.zjy.baseframework.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 禁止重复操作
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface NoRetry {
+public @interface NoRepeatOp {
     int timeout() default 10;
 }
