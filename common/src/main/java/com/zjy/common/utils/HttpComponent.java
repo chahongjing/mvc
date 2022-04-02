8644825d4989dd03d3f54137c143553a546e36e3
zengjunyi <zengjunyi@xiaomi.com>
2022-04-02 14:10:08 +0800

    完善

---
+++ common/src/main/java/com/zjy/common/utils/HttpComponent.java
@@ -0,0 +1,28 @@
+package com.zjy.common.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class HttpComponent {
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(15000);
+        // 设置代理
+        //factory.setProxy(null);
+        return factory;
+    }
+
+    // Map<String, String> vars = Collections.singletonMap("hotel", "42");
+    // String result = restTemplate.getForObject("https://example.com/hotels/{hotel}/rooms/{hotel}", String.class, vars);
+}
