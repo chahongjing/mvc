555d6e1a60d049c88cc6abbdf144ed3d162bab37
zengjunyi <zengjunyi@xiaomi.com>
2022-06-05 15:56:59 +0800

    feat: 优化

--- web/src/main/java/com/zjy/web/contoller/TestController.java
+++ web/src/main/java/com/zjy/web/contoller/TestController.java
@@ -2,17 +2,28 @@ package com.zjy.web.contoller;
 
 import com.zjy.baseframework.enums.BaseResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController extends BaseController {
+    @Value("${mykey1}")
+    private String myKey1;
+    @Value("${mykey2.ab}")
+    private String myKey2;
 
     @GetMapping("/index")
-    public BaseResult<String> index() {
-        return BaseResult.ok();
+    public BaseResult<Map<String, Object>> index() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("myKey1", myKey1);
+        map.put("myKey2", myKey2);
+        return BaseResult.ok(map);
     }
 }
