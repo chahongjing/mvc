fa9995ed98d91085be311e300d776370a17fc01f
zengjunyi <zengjunyi@xiaomi.com>
2022-07-10 18:32:01 +0800

    feat: 优化

--- service/src/main/java/com/zjy/service/configuration/FastJsonConfiguration.java
+++ service/src/main/java/com/zjy/service/configuration/FastJsonConfiguration.java
@@ -2,34 +2,46 @@ package com.zjy.service.configuration;
 
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.zjy.common.utils.FastJsonUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 public class FastJsonConfiguration {
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private FastJsonConfig fastJsonConfig;
     @Bean
-    public HttpMessageConverters customConverters() {
-        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+    public HttpMessageConverters fastJsonMsgConverters() {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         List<MediaType> supportedMediaTypes = new ArrayList<>();
         supportedMediaTypes.add(MediaType.APPLICATION_JSON);
 //        supportedMediaTypes.add(MediaType.TEXT_HTML);
-        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
-        fastConverter.setFastJsonConfig(fastJsonConfig());
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+        converter.setFastJsonConfig(fastJsonConfig);
         //支持text 转string
         StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
-        return new HttpMessageConverters(fastConverter, stringHttpMessageConverter);
+        return new HttpMessageConverters(converter, stringHttpMessageConverter);
     }
-
-    @Bean
-    public FastJsonConfig fastJsonConfig() {
-        // 创建配置类
-        return FastJsonUtils.getFastJsonConfig();
+//    @Bean
+    public HttpMessageConverters jacksonMsgConvert() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+//        supportedMediaTypes.add(MediaType.TEXT_HTML);
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+        converter.setObjectMapper(objectMapper);
+        //支持text 转string
+        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
+        return new HttpMessageConverters(converter, stringHttpMessageConverter);
     }
 }
--- service/src/main/java/com/zjy/service/configuration/JsonConfig.java
+++ service/src/main/java/com/zjy/service/configuration/JsonConfig.java
@@ -1,15 +1,22 @@
 package com.zjy.service.configuration;
 
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.zjy.common.utils.FastJsonUtils;
 import com.zjy.common.utils.JacksonUtil;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JacksonConfig {
-
+public class JsonConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return JacksonUtil::processBuilder;
+    }
+
+    @Bean
+    public FastJsonConfig fastJsonConfig() {
+        // 创建配置类
+        return FastJsonUtils.getFastJsonConfig();
     }
 }
